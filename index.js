86696e25622590bef37ec8a5216b2dbd7d8a15d0
Jonathan Reynolds <j14reynolds@gmail.com>
2016-02-20 17:36:47 -0600

    add sockets

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,6 +12,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/one-click', function (req, res) {
+  io.emit('one-click', tableID);
+});
+
+
+app.get('/double-click', function (req, res) {
+  io.emit('double-click', tableID);
+});
+
+
+app.get('/hold', function (req, res) {
+  io.emit('hold', tableID);
+});
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
@@ -16,5 +33,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
---
+++ public/client.js
@@ -0,0 +1,17 @@
+var socket = io();
+
+// $('form').submit(function(){
+//  socket.emit('chat message', $('#m').val());
+//  $('#m').val('');
+//  return false;
+// });
+
+socket.on('one-click', function(tableID){
+  //Do something
+});
+socket.on('double-click', function(tableID){
+  //Do something
+});
+socket.on('hold', function(tableID){
+  //Do something
+});
