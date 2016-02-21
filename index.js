9b834992dbdf62039d4ffb38a290ad571e757ac9
Jonathan Reynolds <j14reynolds@gmail.com>
2016-02-20 18:01:21 -0600

    Working with sockets

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 app.get('/one-click', function (req, res) {
   io.emit('one-click', tableID);
 });
@@ -26,10 +25,13 @@ app.get('/hold', function (req, res) {
   io.emit('hold', tableID);
 });
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+http.listen(app.get('port'), function(){
+  console.log('listening on *:3000');
 });
